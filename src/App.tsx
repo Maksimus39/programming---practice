6f9db339204ac85c09f158556c7fd2dd37910a28
Macsim Minakov <maksim.minakov.83@mail.ru>
2024-05-07 11:23:58 +0300

    Отрисовка компонент

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {RenderDataIsArray} from "./Рендеринг данных из массивов/RenderDataIsAraay";
+import {FilterArrayElements} from "./Фильтрация массивов элементов/FilterArrayElements";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <RenderDataIsArray/>
+            <br/>
+            <FilterArrayElements/>
+        </div>
+    );
 }
 
 export default App;
