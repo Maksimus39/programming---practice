c467fcb588342ecf4e5e08e7fe81d5c800dad92d
Macsim Minakov <maksim.minakov.83@mail.ru>
2024-05-07 11:24:45 +0300

    Отрисовка компонент

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import {RenderDataIsArray} from "./Рендеринг данных из массивов/RenderDataIsAraay";
 import {FilterArrayElements} from "./Фильтрация массивов элементов/FilterArrayElements";
