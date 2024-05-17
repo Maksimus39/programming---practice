0087826832eac20c645fe40180bcdf8c8cbe1a38
Macsim Minakov <maksim.minakov.83@mail.ru>
2024-05-17 10:59:20 +0300

    Add component

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,11 @@ import React from 'react';
 import './App.css';
 import {RenderDataIsArray} from "./Рендеринг данных из массивов/RenderDataIsAraay";
 import {FilterArrayElements} from "./Фильтрация массивов элементов/FilterArrayElements";
+import {Button} from "./Реагирование на события/Button";
+import Toolbar from "./Реагирование на события/AlertButton";
+import ToolbarTwo from "./Реагирование на события/Toolbar";
+import AppThree from "./Реагирование на события/AppThree";
+import LightSwitch from "./Реагирование на события/LightSwitch";
 
 function App() {
     return (
@@ -9,6 +14,16 @@ function App() {
             <RenderDataIsArray/>
             <br/>
             <FilterArrayElements/>
+            <br/>
+            <Button/>
+            <br/>
+            <Toolbar/>
+            <br/>
+            <ToolbarTwo/>
+            <br/>
+            <AppThree/>
+            <br/>
+            <LightSwitch/>
         </div>
     );
 }
