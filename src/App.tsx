73804c14c72814fb7745d944724df93c4e1f265a
Macsim Minakov <maksim.minakov.83@mail.ru>
2024-06-25 23:58:30 +0300

    попробовал обновить стейт

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import {RenderDataIsArray} from "./Рендеринг данных из массивов/RenderDataIsAraay";
 import {FilterArrayElements} from "./Фильтрация массивов элементов/FilterArrayElements";
@@ -9,29 +9,61 @@ import AppThree from "./Реагирование на события/AppThree";
 import LightSwitch from "./Реагирование на события/LightSwitch";
 import {UseState} from "./usState/UseState";
 import {ConditionalRendering} from "./Conditional Rendering/ConditionalRendering";
+import {Destructuring} from "./DestructuRing/Destructuring";
+import {v1} from "uuid";
+
+
+export type BooksType = {
+    id: string
+    title: string
+    isDone: boolean
+}
 
 function App() {
+
+    const books: BooksType[] = [
+        {id: v1(), title: 'CSS3', isDone: false},
+        {id: v1(), title: 'CSS3', isDone: false},
+        {id: v1(), title: 'CSS3', isDone: false},
+        {id: v1(), title: 'CSS3', isDone: false},
+        {id: v1(), title: 'CSS3', isDone: false}
+    ]
+    const [book, setBook] = useState<BooksType[]>(books)
+
+    function addBook(newTitle: string) {
+        const boc: BooksType = {id: v1(), title: newTitle, isDone: false};
+
+        const newTasks = [boc, ...book]
+        setBook(newTasks)
+        console.log(newTitle)
+    }
+
     return (
         <div className="App">
-            <RenderDataIsArray/>
-            <br/>
-            <FilterArrayElements/>
-            <br/>
-            <Button/>
-            <br/>
-            <Toolbar/>
-            <br/>
-            <ToolbarTwo/>
-            <br/>
-            <AppThree/>
-            <br/>
-            <LightSwitch/>
-            <br/>
-            <UseState/>
-            <br/>
-            <ConditionalRendering led={false}/>
+            {/*<RenderDataIsArray/>*/}
+            {/*<br/>*/}
+            {/*<FilterArrayElements/>*/}
+            {/*<br/>*/}
+            {/*<Button/>*/}
+            {/*<br/>*/}
+            {/*<Toolbar/>*/}
+            {/*<br/>*/}
+            {/*<ToolbarTwo/>*/}
+            {/*<br/>*/}
+            {/*<AppThree/>*/}
+            {/*<br/>*/}
+            {/*<LightSwitch/>*/}
+            {/*<br/>*/}
+            {/*<UseState/>*/}
+            {/*<br/>*/}
+            {/*<ConditionalRendering/>*/}
+            {/*<br/>*/}
 
-            <ConditionalRendering />
+
+            <Destructuring
+                book={book}
+                addBook={addBook}
+            />
         </div>
     );
 }
